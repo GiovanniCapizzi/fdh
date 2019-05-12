f99afc7ba81b687dd7945d31150e3376b530ae00
Giovanni Capizzi <capizzig94@gmail.com>
2019-05-12 15:46:07 +0200

    added some checks

--- fdh/config.py
+++ fdh/config.py
@@ -21,6 +21,18 @@ def make_config(file_helper: IFileHelper,
                 bit_period: float = 1 / 44100,
                 redundancy: int = 2 ) -> Config:
 
+    if not 2<=redundancy<=100:
+        print("Cannot use %d as redundancy value (choose between 2 and 100). Using 2."%(redundancy,))
+        redundancy = 2
+    
+    if not 0 < amplitude <= 1:
+        print("amplitude must be greater than 0 and less or equal to 1. Using 0.0001.")
+        amplitude = 0.0001
+    
+    if not 5e-07 <= bit_period <= 0.1:
+        print("bit_period must be between 5e-07 and 0.1.")
+        bit_period = 1/44100
+
     br = 1 / bit_period  # bit rate, 1MHz per sec
     f0 = br * redundancy
     f1 = br * (redundancy / 2)
